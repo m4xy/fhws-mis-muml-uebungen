2eacf270f32ccde1fc443e29a43cbac601642ee0
Max Arndt <max@eyrichshof.de>
2018-10-18 15:56:41 +0200

    uebung03 1b - not working yet

--- Uebung03/Uebung03.m
+++ Uebung03/Uebung03.m
@@ -1,3 +1,7 @@
+%%Uebung03
+
+%%1a)
+
 %% Time specifications:
 Fs = 128;                   % samples per second
 dt = 1/Fs;                  % seconds per sample
@@ -11,3 +15,10 @@ f2 = sin(32*pi*t)+0.3*cos(48*pi*t);
 figure;
 plot(t,f1);
 plot(t,f2);
+
+%%1b)
+y = fft(f1);
+n = length(y);
+power = abs(y(1:floor(n/2))).^2;
+
+plot(freq,power)
